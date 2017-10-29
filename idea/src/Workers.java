4ab5247f744363344e03e071c38fc80dd7e9c509
Kirill Mudracov <32515326+kirillz732@users.noreply.github.com>
2017-10-29 23:12:01 +0300

    Update Workers.java

--- idea/src/Workers.java
+++ idea/src/Workers.java
@@ -1,5 +1,8 @@
 	//@formatter:on
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.*;
 
 	public class Workers implements Serializable {
 		private String name;
@@ -15,11 +18,11 @@ import java.io.Serializable;
 			this.koef = koef;
 		}
 
-		private double getMoney() throws MyException {
-			if (koef <=0)
-				throw new MyException();
-			return  stavka * (koef * stazh);
+			private double getMoney () throws CoefficientCheckException {
+			if (koef <= 0) throw new CoefficientCheckException();
+			return stavka * (koef * stazh);
 		}
+
 
 		public void setName(String name) {
 			this.name = name;
@@ -50,9 +53,57 @@ import java.io.Serializable;
 			double money =0;
 			try {
 				money = getMoney();
-			} catch (MyException e){
-				e.printStackTrace();
-				System.exit(1);
+			} catch (CoefficientCheckException ex) {
+				ex.printStackTrace();
+
+				ArrayList<Workers> spisok = new ArrayList<Workers>() ;
+				Scanner sc = new Scanner(System.in);
+				System.out.print("Введите количество сотрудников: ");
+				int nomber = sc.nextInt();
+				sc.nextLine();
+
+				for (int i = 0; i < nomber; i++)  {
+					System.out.print("Введен " + (i + 1) + "-й разработчик" + " ФИО: ");
+										String name = sc.nextLine();
+					System.out.print("Введен " + (i + 1) + "-й разработчик" + " Стаж: ");
+					double stazh = sc.nextDouble();
+					sc.nextLine();
+					System.out.print("Введен " + (i + 1) + "-й разработчик" + " Должность: ");
+					String dolzhnost = sc.nextLine();
+					System.out.print("Введен " + (i + 1) + "-й разработчик" + " Коэффициент: ");
+					double koef = sc.nextDouble();
+					sc.nextLine();
+					Workers work = new Workers(name, stazh, dolzhnost, koef);
+					spisok.add(work);
+				}
+				File myFile = new File(  "workers.txt");
+
+				try {
+					FileOutputStream fileOutput = new FileOutputStream(myFile);
+					ObjectOutputStream objectOut = new ObjectOutputStream(fileOutput);
+					for (Workers wor: spisok)
+						System.out.println(wor.toString());
+					objectOut.writeObject(spisok);
+					objectOut.flush();
+					objectOut.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				System.out.println("\n Информация записана");
+				System.out.println("\n Загрузка");
+
+				try {
+					FileInputStream fileInput = new FileInputStream(myFile);
+					ObjectInputStream objectIn = new ObjectInputStream(fileInput);
+					spisok.clear();
+					spisok = (ArrayList<Workers>)objectIn.readObject();
+					for (int i = 0; i < nomber; i++) {
+						System.out.print(spisok.get(i).toString());
+					}
+					objectIn.close();
+				} catch (IOException | ClassNotFoundException e) {
+					e.printStackTrace();
+				}
 			}
 			return " \n Разработчик: " +
 					"\n Имя: " + name  +
