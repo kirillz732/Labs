cc8a48b592dffc41857dfa1b703df3b4bce77444
Kirill Mudracov <32515326+kirillz732@users.noreply.github.com>
2017-10-29 12:19:40 +0300

    Update MyException.java

--- idea/src/MyException.java
+++ idea/src/MyException.java
@@ -3,6 +3,6 @@ import java.io.PrintStream;
 public class MyException extends Exception {
 	@Override
 	public void printStackTrace(PrintStream s) {
-		System.err.println("Ставка не может быть отрицательной или равной нулю");
+		System.err.println("Коэффициент не может быть отрицательной или равной нулю");
 	}
 }
